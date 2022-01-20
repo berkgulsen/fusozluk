050548b0a892f1a4b8bf26a1736f3a6b8ae455e6
berkgulsen <83736291+berkgulsen@users.noreply.github.com>
2022-01-20 18:35:28 +0300

    profile update pages edited

--- resources/views/widget/topnavwidget.blade.php
+++ resources/views/widget/topnavwidget.blade.php
@@ -2,7 +2,6 @@
     <!-- Top navigation-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
         <div class="container-fluid">
-            <button class="btn btn-primary" id="sidebarToggle">Menü</button>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
@@ -11,7 +10,7 @@
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{\Illuminate\Support\Facades\Auth::user()->name}}</a>
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#!">Profil</a>
+                                <a class="dropdown-item" href="{{route('profile.show')}}">Profil</a>
                                 <a class="dropdown-item" href="{{route('logout')}}">Çıkış Yap</a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#!">Something else here</a>
